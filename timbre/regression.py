c53fa3b1d8f40b5a3e20dd814c4a7e1b4d515231
Jonathan Alvarez-Gutierrez <jagthebeetle@gmail.com>
2019-12-15 00:02:13 -0500

    linted

--- timbre/regression.py
+++ timbre/regression.py
@@ -11,58 +11,46 @@ from tensorflow.keras import layers
 
 dataset = pd.read_csv('aligned.csv')
 
-train_dataset = dataset.sample(frac=0.8,random_state=0)
+train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
-train_labels = train_dataset.pop('key_conf')
-test_labels = test_dataset.pop('key_conf')
+train_labels = train_dataset.pop('key_conf') * 100
+test_labels = test_dataset.pop('key_conf') * 100
 
 # Normalize data
 train_stats = train_dataset.describe().transpose()
-print(dataset.describe())
-quit()
+
+
 def norm(x):
-  return (x - train_stats['mean']) / train_stats['std']
+    return (x - train_stats['mean']) / train_stats['std']
+
 
 normed_train_data = norm(train_dataset)
 normed_test_data = norm(test_dataset)
 
 # Model
-def build_model():
-  model = keras.Sequential([
-    layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=[
+        len(train_dataset.keys())]),
     layers.Dense(64, activation='relu'),
     layers.Dense(1)
-  ])
-  optimizer = tf.keras.optimizers.RMSprop(0.001)
+])
+optimizer = tf.keras.optimizers.RMSprop(0.001)
 
-  model.compile(loss='mse',
-                optimizer=optimizer,
-                metrics=['mae', 'mse'])
-  return model
-model = build_model()
+model.compile(loss='mse',
+              optimizer=optimizer,
+              metrics=['mae', 'mse'])
 
-# train
+# Train with TensorBoard logging
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir, histogram_freq=1)
 
-log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-EPOCHS = 1000
+EPOCHS = 50
 history = model.fit(
-  normed_train_data, train_labels,
-  epochs=EPOCHS, validation_split = 0.2, callbacks=[tensorboard_callback])
+    normed_train_data, train_labels,
+    epochs=EPOCHS, validation_split=0.2, callbacks=[tensorboard_callback],
+    verbose=0)
 
 # eval
 loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
-print("Testing set Mean Abs Error: {:5.2f} confidence".format(mae))
-
-# predict
-test_predictions = model.predict(normed_test_data).flatten()
-
-a = plt.axes(aspect='equal')
-plt.scatter(test_labels, test_predictions)
-plt.xlabel('True Values [key_conf]')
-plt.ylabel('Predictions [key_conf]')
-lims = [0, 1]
-plt.xlim(lims)
-plt.ylim(lims)
-_ = plt.plot(lims, lims)
-plt.show()
+print("Testing MAE: {:5.2f} key confidence".format(mae))
